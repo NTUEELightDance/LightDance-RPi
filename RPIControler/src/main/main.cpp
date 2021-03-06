38ffb36610b5b45cba9a1cbeadcc6255e81ed98c
Wish <b08901181@ntu.edu.tw>
2021-03-07 00:23:09 +0800

    add c++ output

--- RPIControler/src/main/main.cpp
+++ RPIControler/src/main/main.cpp
@@ -14,8 +14,13 @@ using namespace std;
 //----------------------------------------------------------------------
 //    Global cmd Manager
 //----------------------------------------------------------------------
-CmdParser *cmdMgr = new CmdParser("");
-
+CmdParser* cmdMgr = new CmdParser("");
+/*
+void sendToRPIClient(const bool& ok, const string& msg) {
+    //const string = "{\"OK\":1,\"msg\":\"Start Playing\"}";
+    cout << "{\"OK\":" ok << ",\"msg\":\"" << msg << "\"}";
+}
+ */
 extern bool initCommonCmd();
 extern bool initRPICmd();
 extern RPIMgr rpiMgr;
@@ -26,12 +31,12 @@ size_t startTime = 0;
 //
 
 //*
-void sigHandler(int sig)
-{
-   rpiMgr.pause();
-   cout << "startTime: " << rpiMgr.getStartTime() << endl;
+void sigHandler(int sig) {
+    rpiMgr.pause();
+    //cout << "startTime: " << rpiMgr.getStartTime() << endl;
 }
 //*/
+
 
 static void
 usage()
@@ -46,28 +51,24 @@ myexit()
    exit(-1);
 }
 
-int main(int argc, char **argv)
+int
+main(int argc, char** argv)
 {
    ifstream dof;
-   signal(SIGINT, sigHandler);
-   if (argc == 3)
-   { // -file <doFile>
-      if (myStrNCmp("-File", argv[1], 2) == 0)
-      {
-         if (!cmdMgr->openDofile(argv[2]))
-         {
+    signal(SIGINT, sigHandler);
+   if (argc == 3) {  // -file <doFile>
+      if (myStrNCmp("-File", argv[1], 2) == 0) {
+         if (!cmdMgr->openDofile(argv[2])) {
             cerr << "Error: cannot open file \"" << argv[2] << "\"!!\n";
             myexit();
          }
       }
-      else
-      {
+      else {
          cerr << "Error: unknown argument \"" << argv[1] << "\"!!\n";
          myexit();
       }
    }
-   else if (argc != 1)
-   {
+   else if (argc != 1) {
       cerr << "Error: illegal number of argument (" << argc << ")!!\n";
       myexit();
    }
@@ -76,11 +77,10 @@ int main(int argc, char **argv)
       return 1;
 
    CmdExecStatus status = CMD_EXEC_DONE;
-   cout << "start 1" << endl;
-   while (status != CMD_EXEC_QUIT)
-   { // until "quit" or command error
+    cout << "start 1" << endl;
+   while (status != CMD_EXEC_QUIT) {  // until "quit" or command error
       status = cmdMgr->execOneCmd();
-      cout << endl; // a blank line between each command
+      cout << endl;  // a blank line between each command
    }
 
    return 0;
