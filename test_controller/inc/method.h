07e789624c47e404c7435853c7c6a7b935eb0039
傅譽的MacBook <b08901039@ntu.edu.tw>
2021-12-21 21:10:55 +0800

    Move logic to method.cpp

--- test_controller/inc/method.h
+++ test_controller/inc/method.h
@@ -15,150 +15,75 @@ extern RPiMgr *rpiMgr;
 // You must implement the method() function in child classes
 class BaseMethod {
 public:
-    BaseMethod(zmq::socket_t& socket):_socket(socket){};
-    void exec(const vector<string>& cmd, bool& quit){
-        thread t(&(BaseMethod::method), this, cmd, quit);
-        // Seperate the execution
-        t.detach();
-    }
+    BaseMethod(zmq::socket_t& socket);
+    void exec(const vector<string>& cmd, bool& quit);
 protected:
-    virtual void method(const vector<string>& cmd, bool& quit){
-        string err = "Error: not implementing method()\n";
-        send_str(_socket, err);
-    };
+    virtual void method(const vector<string>& cmd, bool& quit);
 
     zmq::socket_t& _socket;
 };
 
 class Load: public BaseMethod {
 public:
-    Load(zmq::socket_t& socket): BaseMethod(socket){};
+    Load(zmq::socket_t& socket);
 protected:
-    void method(const vector<string>& cmd, bool& quit){
-        if (cmd.size() > 1)
-            rpiMgr->load(cmd[1]);
-        else
-            rpiMgr->load();
-    };
+    void method(const vector<string>& cmd, bool& quit);
 };
 
 class Play: public BaseMethod {
 public:
-    Play(zmq::socket_t& socket): BaseMethod(socket){};
+    Play(zmq::socket_t& socket);
 protected:
-    void method(const vector<string>& cmd, bool& quit){
-        if (cmd.size() == 1)
-            rpiMgr->play(false, 0);
-        else if (cmd.size() == 2){
-            unsigned startTime;
-            if (!Str2Unsint(cmd[1], startTime)){
-                string err = "Error: illegal option \"" + cmd[1] +"\"\n";
-                _socket.send(err.c_str(), err.size());
-            }
-            else
-                rpiMgr->play(true, startTime);
-        }
-        else if (cmd.size() == 3){
-            unsigned startTime, delayTime;
-            const bool option1 = Str2Unsint(cmd[1], startTime), option2 = Str2Unsint(cmd[2], delayTime);
-            if (!option1){
-                string err = "Error: illegal option \"" + cmd[1] + "\"\n";
-                send_str(_socket, err);
-            }
-            if (!option2){
-                string err = "Error: illegal option \"" + cmd[2] + "\"\n";
-                send_str(_socket, err);
-            }
-            if (option1 && option2)
-                rpiMgr->play(true, startTime, delayTime);
-        }
-    };
+    void method(const vector<string>& cmd, bool& quit);
 };
 
 class Stop: public BaseMethod {
 public:
-    Stop(zmq::socket_t& socket): BaseMethod(socket){};
+    Stop(zmq::socket_t& socket);
 protected:
-    void method(const vector<string>& cmd, bool& quit){
-        rpiMgr->stop();
-    }
+    void method(const vector<string>& cmd, bool& quit);
 };
 
 class StatusLight: public BaseMethod {
 public:
-    StatusLight(zmq::socket_t& socket): BaseMethod(socket){};
+    StatusLight(zmq::socket_t& socket);
 protected:
-    void method(const vector<string>& cmd, bool& quit){
-        rpiMgr->statuslight();
-    }
+    void method(const vector<string>& cmd, bool& quit);
 };
 
 class Eltest: public BaseMethod {
 public:
-    Eltest(zmq::socket_t& socket): BaseMethod(socket){};
+    Eltest(zmq::socket_t& socket);
 protected:
-    void method(const vector<string>& cmd, bool& quit){
-        // default all light
-        if (cmd.size() == 1)
-            rpiMgr->eltest(-1, 4095);
-        // id/brightness
-        else if (cmd.size() >= 3){
-            int id = stoi(cmd[1]);
-            string msg = "testing id: " + id;
-            send_str(_socket, msg);
-
-            unsigned brightness;
-            if (!Str2Unsint(cmd[2], brightness)){
-                string err = "Error: illegal option \"" + cmd[2] + "\"\n";
-                send_str(_socket, msg);
-            }
-            else
-                rpiMgr->eltest((int)id, brightness);
-        }
-        // error
-        else{
-            string err = "Error: missing options, should give id(0 ~ 31) and brightness(0 ~ 4095)\n";
-            send_str(_socket, err);
-        }
-    }
+    void method(const vector<string>& cmd, bool& quit);
 };
 
 class Ledtest: public BaseMethod {
 public:
-    Ledtest(zmq::socket_t& socket): BaseMethod(socket){};
+    Ledtest(zmq::socket_t& socket);
 protected:
-    void method(const vector<string>& cmd, bool& quit){
-        rpiMgr->ledtest();  // TODO: need to get cmd arguments
-    }
+    void method(const vector<string>& cmd, bool& quit);
 };
 
 class List: public BaseMethod {
 public:
-    List(zmq::socket_t& socket): BaseMethod(socket){};
+    List(zmq::socket_t& socket);
 protected:
-    void method(const vector<string>& cmd, bool& quit){
-        rpiMgr->list();
-    }
+    void method(const vector<string>& cmd, bool& quit);
 };
 
 class Quit: public BaseMethod {
 public:
-    Quit(zmq::socket_t& socket): BaseMethod(socket){};
+    Quit(zmq::socket_t& socket);
 protected:
-    void method(const vector<string>& cmd, bool& quit){
-        rpiMgr->quit();
-        quit = true;
-    }
+    void method(const vector<string>& cmd, bool& quit);
 };
 
 class Pause: public BaseMethod {
 public:
-    Pause(zmq::socket_t& socket): BaseMethod(socket){};
+    Pause(zmq::socket_t& socket);
 protected:
-    void method(const vector<string>& cmd, bool& quit){
-        cout << "pause success" << endl;
-        rpiMgr->pause();
-    }
+    void method(const vector<string>& cmd, bool& quit);
 };
 
 #endif
