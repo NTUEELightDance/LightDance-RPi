1ea8119ae85766a283c447a9508e9f525dd2178a
rayray2002 <rayray2002.huang@gmail.com>
2021-12-21 13:56:31 +0800

    RPi-#19. Add a zmq socket object for python

---
+++ test/reciever.py
@@ -0,0 +1,17 @@
+import time
+
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:8000")
+
+while True:
+    #  Wait for next request from client
+    message = socket.recv()
+    print("Received request: %s" % message.decode())
+
+    time.sleep(1)
+
+    #  Send reply back to client
+    socket.send_string(f"Success: {message.decode()}")
