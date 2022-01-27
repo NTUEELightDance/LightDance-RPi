91f300ea3c3103ae862268d2a85bc74330d0ef98
Richie FY <59409544+FYRichie@users.noreply.github.com>
2022-01-27 14:23:37 +0800

    remove redundant sudo (#36)

--- scripts/install_zeromq_rpi.sh
+++ scripts/install_zeromq_rpi.sh
@@ -5,8 +5,8 @@ sudo apt-get install libtool pkg-config build-essential autoconf automake
 curl https://download.libsodium.org/libsodium/releases/libsodium-1.0.18.tar.gz --output libsodium-1.0.18.tar.gz
 tar -zxvf libsodium-1.0.18.tar.gz
 cd libsodium-1.0.18/
-sudo ./configure
-sudo make
+./configure
+make
 sudo make install
 cd ../
 
@@ -15,8 +15,8 @@ cd ../
 wget https://archive.org/download/zeromq_4.1.4/zeromq-4.1.4.tar.gz
 tar -zxvf zeromq-4.1.4.tar.gz
 cd zeromq-4.1.4/
-sudo ./configure
-sudo make
+./configure
+make
 sudo make install
 cd /usr/include/
 sudo wget https://raw.githubusercontent.com/zeromq/cppzmq/master/zmq.hpp
@@ -24,9 +24,9 @@ sudo wget https://raw.githubusercontent.com/zeromq/cppzmq/master/zmq.hpp
 # Remove no use folders and files
 cd /home/pi/LightDance-RPi/scripts
 rm libsodium-1.0.18.tar.gz
-sudo rm -rf libsodium-1.0.18
+rm -rf libsodium-1.0.18
 rm zeromq-4.1.4.tar.gz
-sudo rm -rf zeromq-4.1.4
+rm -rf zeromq-4.1.4
 
 # set include file
 sudo echo /etc/ld.so.conf >> /usr/local/lib/libzmq.so.5
