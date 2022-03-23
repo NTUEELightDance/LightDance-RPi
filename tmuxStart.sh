741d3e9f4d1ca15b4ebeade13f20bc49e2e107f7
rayray2002 <rayray2002.huang@gmail.com>
2022-03-23 22:29:05 +0800

    RPi-#80. tmux

--- tmuxStart.sh
+++ tmuxStart.sh
@@ -3,6 +3,6 @@ echo $DANCERNAME
 
 tmux new-session -d -s lightdance
 
-new-window ~/LightDance-RPi/startCLI.sh
-new-window ~/LightDance-RPi/startClient.sh
-new-window ~/LightDance-RPi/startController.sh
+tmux new-window ~/LightDance-RPi/startCLI.sh
+tmux new-window ~/LightDance-RPi/startClient.sh
+tmux new-window ~/LightDance-RPi/startController.sh
