8ef720131addb9172f375f0dde4249cb11ebd278
rayray2002 <rayray2002.huang@gmail.com>
2022-03-24 03:06:19 +0800

    RPi-#86. readme

--- method/boardInfo.py
+++ method/boardInfo.py
@@ -1,23 +1,25 @@
 import os
 import socket
-import sys
 
 from .baseMethod import BaseMethod
+
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+
+def get_hostname():
+    hostname = socket.getfqdn(socket.gethostname())
+    return hostname
 
 
 # BoardInfo
 class BoardInfo(BaseMethod):
     def method(self, payload):
-        # info = [
-        #     "boardInfo",
-        #     {"name": os.name, "type": "dancer", "OK": True, "msg": "Success"},
-        # ]
-        # return info
-        if len(sys.argv) == 2:
-            dancerName = sys.argv[1]
-        else:
-            dancerName = "10_dontstop"
-
         try:
             ip = get_ip()
         except socket.gaierror:
