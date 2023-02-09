6bfc446b804d6bb36b4efbfa366b0ca096c12870
lin-1214 <b10901059@g.ntu.edu.tw>
2023-02-09 11:45:43 +0800

    RPi-#140 Use Serialization to load data in OFPlayer

---
+++ 2023/commands/setup/test_ofplayer/OFPlayer.h
@@ -0,0 +1,85 @@
+#ifndef OF_PLAYER
+#define OF_PLAYER
+
+#include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <atomic>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+// serialize
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/tmpdir.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/version.hpp>
+
+using namespace std;
+
+
+struct OFStatus {
+    int r, g, b, a;
+    OFStatus();
+    OFStatus(const int &_r, const int &_g, const int &_b, const int &_a);
+
+    template <class Archive>;
+    void serialize(Archive &archive, const unsigned int version);
+}
+
+struct OFFrame {
+    int start;
+    bool fade;
+    vector<pair<string, OFStatus>> statusList;
+    OFFrame();
+    OFFrame(const int &_start, const bool &_fade, const vector<pair<string, OFStatus>> &_statusList );
+
+    template <class Archive>
+    void serialize(Archive &archive, const unsigned int version);
+}
+
+class OFPlayer {
+    public:
+        OFPlayer();
+        OFPlayer(const int &_fps, const vector<OFFrame> &_frameList, const vector<vector<OFStatus>> &_statusList)
+        // threading function
+        void loop(const bool *playing, const timeval *baseTime);
+
+        template <class Archive>
+        void serialize(Archive &archive, const unsigned int version);
+        string list() const;
+    
+    private:
+        int fps;
+        int frameId;
+        vector<OFFrame> frameList;
+        vector<vector<OFStatus>> statusList;
+        unordered_map<string, int> channelIds;
+
+        // functions for finding frame at specific time
+        long getElapsedTime(const struct timeval &base,
+                            const struct timeval &current);
+        vector<OFStatus> findFrameStatus(const long &time);
+        vector<OFStatus> findFadeFrameStatus(const long &time);
+        int findFrameId(const long &time);
+        int findChannelId(const string &partName);
+
+
+        // serialization
+        friend class boost::serialization::access;
+        friend ostream &operator<<(ostream &os, const OFPlayer &player);
+};
+
+void saveOFPlayer(OFPlayer &player, const char *filename);
+bool restoreOFPlayer(OFPlayer &player, const char *filename);
+
+
+#endif
