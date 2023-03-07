aac0a1c81d54c09261a3b1a7bb989062d4db46aa
rayray2002 <rayray2002.huang@gmail.com>
2023-03-07 21:29:39 +0800

    update something more

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+# Create root content folder
+sudo mkdir -p /lightdance
+sudo mkdir -p /lightdance/config
+sudo mkdir -p /lightdance/data
+
 # Build controller
 cd controller
 mkdir -p build && cd build
@@ -11,4 +16,9 @@ sudo cp -r bin/* /usr/local/bin/
 sudo chown root /usr/local/bin/*
 sudo chmod u+s /usr/local/bin/*
 
-# Bui
+# Copy services
+sudo cp -r system/* /etc/systemd/system/
+sudo systemctl daemon-reload
+
+# Copy configuration files
+sudo cp ws_client/config.py /lightdance/config/
