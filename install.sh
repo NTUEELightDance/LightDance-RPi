ca0b87adccca54d88ae0744437e88e2d133749f7
tom1484 <tomchen2003611@gmail.com>
2023-03-09 20:08:34 +0800

    fix wrong dir in installation script

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ sudo rm -rf /usr/local/bin/*
 sudo cp -r bin/* /usr/local/bin/
 sudo chown root /usr/local/bin/*
 sudo chmod u+s /usr/local/bin/*
+cd ..
 
 # Copy services
 sudo cp -r system/* /etc/systemd/system/
