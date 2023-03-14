210e1f8118b4f32fa3542b6276d7b9be0001de87
Ray Huang <rayray2002.huang@gmail.com>
2023-03-15 03:40:52 +0800

    RPi-210. Modify new protocol with control server (#214)

--- ws_client/client.py
+++ ws_client/client.py
@@ -13,69 +13,119 @@ from ntpclient import *
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 
-LED_SAVE_DIR = "./data/LED.json"
-OF_SAVE_DIR = "./data/OF.json"
+DATA_SAVE_DIR = "/Users/ray/Desktop/lightdance/LightDance-RPi-ray/data"
 
-DATA_SAVE_DIR = "/home/pi/LightDance-RPi/data"
 
 class Client:
     def __init__(self):
         super(Client, self).__init__()
         self.url = f"ws://{SERVER_IP}:{SERVER_PORT}"
         self.action = ""
-        self.paylaod = {}
-        self.ntpclient = NTPClient()
+        self.payload = {}
+        self.ntp_client = NTPClient()
+        self.MAC = ":".join(("%012X" % get_mac())[i : i + 2] for i in range(0, 12, 2))
+        self.ws = None
 
-    def startclient(self):
+    def start_client(self):
+        print(f"connecting to {self.url}")
         while True:
             try:
-                ws = websocket.WebSocketApp(
+                self.ws = websocket.WebSocketApp(
                     self.url,
-                    on_message=self.on_message,
                     on_open=self.on_open,
+                    on_message=self.on_message,
                     on_close=self.on_close,
+                    on_error=self.on_error,
                 )
-                ws.run_forever()
+                self.ws.run_forever()
                 time.sleep(3)
             except websocket.WebSocketException:
                 print("Failed to connect")
 
-    def on_message(self, ws, message):
-        action, payload = self.ParseServerData(message)
+    def send_response(self, topic: str, statusCode: int, payload: dict):
+        message = json.dumps(
+            {
+                "from": "RPi",
+                "topic": topic,
+                "statusCode": statusCode,
+                "payload": payload,
+            }
+        )
+        print("Send message to server:")
+        print(message)
+        self.ws.send(message)
 
-        if action == "command":
+    def on_open(self, ws):
+        print("Successfully on_open")
+        print(f"MAC address: {self.MAC}")
+        self.send_response("boardInfo", 0, {"MAC": self.MAC})
+        print("Mac address sent")
+
+    def on_close(self, ws):
+        print("websocket closed")
+
+    def on_error(self, ws, error):
+        print("The error is %s" % error)
+
+    def parse_server_data(self, message):
+        print("Message from server:")
+        print(message)
+        try:
+            message = json.loads(message)
+            topic = message["topic"]
+            payload = message["payload"]
+            print(topic, payload)
+            return topic, payload
+        except:
+            print("Invalid json format:")
+            print(message)
+
+    def on_message(self, ws, message):
+        topic, payload = self.parse_server_data(message)
+
+        if topic == "command":
             print("execute payload:")
             print(payload)
             status = -1
             message_to_server = ""
-            
+
             try:
-                subp = subprocess.Popen(payload, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                outs, errs = subp.communicate()
+                process = subprocess.Popen(
+                    payload, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                )
+                outs, errs = process.communicate()
                 outs = outs.decode()
                 errs = errs.decode()
-                status = subp.poll()
+                status = process.poll()
                 if status == 0:
                     message_to_server = outs
                     print("Subprocess Success")
-                    
+
                 else:
                     message_to_server = errs
                     print("Subprocess Error")
                     print("Error message:")
                     print(errs)
             except:
+                print("hi")
                 print("Unable to run Subprocess")
                 message_to_server = "Unable to run Subprocess"
-            
-            self.parse_response(ws, payload[0], status, message_to_server)
 
-        elif action == "upload":
+            command = [str(w) for w in payload]
+            payload_to_server = {
+                "MAC": self.MAC,
+                "command": " ".join(command),
+                "message": message_to_server,
+            }
+            print(payload_to_server)
+            self.send_response("command", status, payload_to_server)
+
+        elif topic == "upload":
             print("upload")
             try:
                 os.mkdir(DATA_SAVE_DIR)
             except:
-                print("dirctory /lightdance exist")
+                print(f"directory {DATA_SAVE_DIR} exist")
 
             print(os.path.join(DATA_SAVE_DIR, "control.json"))
             with open(os.path.join(DATA_SAVE_DIR, "control.json"), "w") as f:
@@ -92,87 +142,42 @@ class Client:
             status = -1
 
             try:
-                subp = subprocess.Popen(["load"])
+                process = subprocess.Popen(["load"])
                 message_to_server = "success"
-                status = subp.poll()
+                status = process.poll()
                 print("load complete")
-                
+
             except:
-                print("can not run subprocess load")
+                print("Can't run subprocess load")
                 message_to_server = "can not run subprocess load"
                 status = -1
-        
-            self.parse_response(ws, action, status, message_to_server)
 
-        elif action == "sync":
+            self.send_response(
+                "command",
+                status,
+                {
+                    "MAC": self.MAC,
+                    "command": "load",
+                    "message": message_to_server,
+                },
+            )
+
+        elif topic == "sync":
             print("sync")
 
         else:
             print("Invalid action")
-            self.parse_response(ws, action, -1, "command not found")
-
-    def ParseServerData(self, message):
-        print("Message from server:")
-        print(message)
-        try:
-            message = json.loads(message)
-            action = message["action"]
-            payload = message["payload"]
-            print(action, payload)
-            return action, payload
-        except:
-            print("Invalid json format:")
-            print(message)
-
-    def parse_response(self, ws, command, status, message: str):
-        
-        ws.send(
-            json.dumps(
+            self.send_response(
+                topic,
+                -1,
                 {
-                    "command":command,
-                    "status": status,
-                    "payload": {
-                        "type": "RPi",
-                        "info":{
-                            "message": message
-                        }
-                    }
-                }
+                    "MAC": self.MAC,
+                    "command": " ".join(payload),
+                    "message": "Invalid action",
+                },
             )
-        )
-        print("Send message to server completed")
-        print("message:{}".format(message))
-
-    def on_open(self, ws):
-        print("Successfully on_open")  # Print Whether successfully on_open
-        macaddr = ':'.join(("%012X" % get_mac())[i:i+2] for i in range(0, 12, 2))
-        print(macaddr)
-        ws.send(
-            json.dumps(
-                {
-                    "command":"boardInfo",
-                    "status": 0,
-                    "payload": 
-                    {
-                        "type":"RPi",
-                        "info":{
-                            "macaddr": macaddr
-                        }
-                    }
-                }
-            )
-        )
-        print("Mac address sent")
-
-    def on_close(self, ws):
-        print("websocket closed")
-
-    ####### on_error ########
-    # def on_error(self,ws,error):
-    #     print("The error is %s" %error)
 
 
 if __name__ == "__main__":
-    print("Start")
     Test = Client()
-    Test.startclient()
+    Test.start_client()
