3516e349817ba5bf961c388d26c6e1ebbefb56d1
ArthurNieh <94263205+ArthurNieh@users.noreply.github.com>
2024-02-26 21:42:20 +0800

    Revert "Rpi #242 " (#248)

--- controller/software/src/commands/player.cpp
+++ controller/software/src/commands/player.cpp
@@ -1,42 +1,31 @@
 #include "player.h"
 
 #include "const.h"
-
-using namespace boost;
 string Player::getDancerName() { return dancerName; };
 
-bool Player::setDancer(const string& _dancerName, property_tree::ptree& dancerData) {
+bool Player::setDancer(const string &_dancerName, json &dancerData) {
     // name
     dancerName = _dancerName;
     // FPS
 
-    fps = dancerData.get<int>("fps");
+    fps = int(dancerData["fps"]);
 
     // LED
-
     LEDPARTS.clear();
-    for (property_tree::ptree::const_iterator it = dancerData.get_child("LEDPARTS").begin();
-        it != dancerData.get_child("LEDPARTS").end(); ++it) {
+    for (json::iterator it = dancerData["LEDPARTS"].begin();
+         it != dancerData["LEDPARTS"].end(); ++it) {
         LEDStripeSetting newSetting;
-        newSetting.len = it->second.get<int>("len");
-        newSetting.id = it->second.get<int>("id");
-        std::string led_name = it->first;
-        LEDPARTS[led_name] = newSetting;
+        newSetting.len = it.value()["len"];
+        newSetting.id = it.value()["id"];
+        LEDPARTS[it.key()] = newSetting;
     }
-    // for (property_tree::ptree::const_iterator it = dancerData.get_child("LEDPARTS").data().begin();
-    //      it != dancerData.get_child("LEDPARTS").data().end(); ++it) {
-    //     LEDStripeSetting newSetting;
-    //     newSetting.len = it.value()["len"];
-    //     newSetting.id = it.value()["id"];
-    //     LEDPARTS[it.key()] = newSetting;
-    // }
 
     // OF
 
-    // OFPARTS.clear();
-    // for (json::iterator it = dancerData["OFPARTS"].begin();
-    //      it != dancerData["OFPARTS"].end(); ++it)
-    //     OFPARTS[it.key()] = it.value();
+    OFPARTS.clear();
+    for (json::iterator it = dancerData["OFPARTS"].begin();
+         it != dancerData["OFPARTS"].end(); ++it)
+        OFPARTS[it.key()] = it.value();
 
     // hardware
 
@@ -46,11 +35,11 @@ bool Player::setDancer(const string& _dancerName, property_tree::ptree& dancerDa
 string Player::list() const {
     stringstream ostr;
     ostr << "******************************\ndancerName:  " << dancerName
-        << "\n";
+         << "\n";
     ostr << "fps:  " << fps << "\n";
     ostr << "OFPARTS:\n\n";
     for (unordered_map<string, int>::const_iterator it = OFPARTS.begin();
-        it != OFPARTS.end(); ++it) {
+         it != OFPARTS.end(); ++it) {
         string part = "    ";
         part += it->first;
         for (int i = 0; i < max(short(20 - it->first.size()), (short)0); ++i) {
@@ -62,10 +51,10 @@ string Player::list() const {
 
     ostr << "\nLEDPARTS:\n\n";
     for (unordered_map<string, LEDStripeSetting>::const_iterator it =
-        LEDPARTS.begin();
-        it != LEDPARTS.end(); ++it) {
+             LEDPARTS.begin();
+         it != LEDPARTS.end(); ++it) {
         ostr << it->first + ":{\n    id: " + to_string(it->second.id) +
-            ",\n    len: " + to_string(it->second.len) + "\n}\n";
+                    ",\n    len: " + to_string(it->second.len) + "\n}\n";
     }
     ostr << "******************************\0";
 
@@ -73,23 +62,23 @@ string Player::list() const {
 }
 
 template <class Archive>
-void Player::serialize(Archive& archive, const unsigned int version) {
-    archive& dancerName;
-    archive& fps;
-    archive& OFPARTS;
-    archive& LEDPARTS;
-    archive& myLEDPlayer;
-    archive& myOFPlayer;
-    archive& LEDloaded;
-    archive& OFloaded;
+void Player::serialize(Archive &archive, const unsigned int version) {
+    archive &dancerName;
+    archive &fps;
+    archive &OFPARTS;
+    archive &LEDPARTS;
+    archive &myLEDPlayer;
+    archive &myOFPlayer;
+    archive &LEDloaded;
+    archive &OFloaded;
 };
 
-ostream& operator<<(ostream& ostream, const Player& player) {
+ostream &operator<<(ostream &ostream, const Player &player) {
     ostream << player.list();
     return ostream;
 }
 
-void savePlayer(const Player& savePlayer, const char* filename) {
+void savePlayer(const Player &savePlayer, const char *filename) {
     // make an archive
     ofstream ofs(filename);
     if (!ofs) {
@@ -100,7 +89,7 @@ void savePlayer(const Player& savePlayer, const char* filename) {
     oa << savePlayer;
 }
 
-bool restorePlayer(Player& savePlayer, const char* filename) {
+bool restorePlayer(Player &savePlayer, const char *filename) {
     // open the archive
     ifstream ifs(filename);
     if (!ifs) {
@@ -117,7 +106,7 @@ bool restorePlayer(Player& savePlayer, const char* filename) {
     return true;
 }
 
-void LEDload(Player& Player, property_tree::ptree& data_json) {
+void LEDload(Player &Player, json &data_json) {
     vector<vector<LEDFrame>> frameLists;
     vector<int> stripShapes;
 
@@ -130,69 +119,59 @@ void LEDload(Player& Player, property_tree::ptree& data_json) {
     frameLists.resize(partNum);
 
     for (unordered_map<string, LEDStripeSetting>::iterator part_it =
-        Player.LEDPARTS.begin();
-        part_it != Player.LEDPARTS.end(); ++part_it) {
+             Player.LEDPARTS.begin();
+         part_it != Player.LEDPARTS.end(); ++part_it) {
         const string partName = part_it->first;
         const unsigned int id = part_it->second.id;
         const unsigned int len = part_it->second.len;
 
-        stripShapes[id] = len;
-        auto check = data_json.get_child_optional(partName);
-        if (!check) {
-            cerr << "Error in LED data: " << partName << " not found" << endl;
-        }
-        const auto darkFrame = LEDFrame(0, false, len);
-        // If no frames are given, push only dark frame
+        const json &frames_json = data_json[partName];
 
-        if (check->size() == 0) {
-            cerr << "warning" << partName << " has no frames" << endl;
+        stripShapes[id] = len;
+
+        // If no frames are given, push only dark frame
+        const LEDFrame darkFrame(0, false, len);
+        if (frames_json.size() == 0) {
             frameLists[id].push_back(darkFrame);
             continue;
         }
+
         // If not starting at time = 0, use dark frame as first frame
-        const int firstStartTime = data_json.get_child(partName).front().second.get<int>("start");
+        const int firstStartTime = frames_json.begin().value()["start"];
         if (firstStartTime != 0) {
             frameLists[id].push_back(darkFrame);
         }
 
         vector<LEDStatus> statusList;
-        statusList.clear();
-
-        for (property_tree::ptree::value_type& frame_data : data_json.get_child(partName)) {
-            property_tree::ptree frame_json = frame_data.second;
-            if (!frame_json.get_child_optional("status")) {
-                cerr << "Error in LED data: " << partName << " has no status key" << endl;
-            }
-            if (frame_json.get_child("status").size() != len) {
-                cerr << "Error in LED data: " << partName << " has size mismatch" << endl;
+        for (const json &frame_json : frames_json) {
+            if (frame_json["status"].size() != len) {
+                // TODO: print size mismatch warning
+                continue;
             }
             statusList.clear();
-            for (property_tree::ptree::value_type& status_data : frame_json.get_child("status")) {
-                property_tree::ptree status_json = status_data.second;
-                auto it = status_json.begin();
-                statusList.push_back(LEDStatus(
-                    it->second.get_value<int>(),
-                    (++it)->second.get_value<int>(),
-                    (++it)->second.get_value<int>(),
-                    (++it)->second.get_value<int>()));
+            for (auto &status_json : frame_json["status"]) {
+                statusList.push_back(LEDStatus(status_json[0], status_json[1],
+                                               status_json[2],
+                                               (int)status_json[3] * 10));
             }
-            frameLists[id].push_back(LEDFrame(frame_json.get<int>("start"), frame_json.get<bool>("fade"), statusList));
+
+            frameLists[id].push_back(
+                LEDFrame(frame_json["start"], frame_json["fade"], statusList));
         }
+    }
 
+    Player.myLEDPlayer = LEDPlayer(Player.fps, frameLists, stripShapes);
+    Player.LEDloaded = true;
 
-        Player.myLEDPlayer = LEDPlayer(Player.fps, frameLists, stripShapes);
-        Player.LEDloaded = true;
-
-        vector<vector<int>>& currentStatus = Player.myLEDPlayer.currentStatus;
-        currentStatus.resize(partNum);
-        for (int i = 0; i < partNum; i++) {
-            currentStatus[i].resize(stripShapes[i]);
-            fill(currentStatus[i].begin(), currentStatus[i].end(), 0);
-        }
+    vector<vector<int>> &currentStatus = Player.myLEDPlayer.currentStatus;
+    currentStatus.resize(partNum);
+    for (int i = 0; i < partNum; i++) {
+        currentStatus[i].resize(stripShapes[i]);
+        fill(currentStatus[i].begin(), currentStatus[i].end(), 0);
     }
 }
 
-void OFload(Player& Player, property_tree::ptree& data_json) {
+void OFload(Player &Player, json &data_json) {
     vector<OFFrame> frameLists;
     vector<vector<OFStatus>> statusList;
 
@@ -205,40 +184,35 @@ void OFload(Player& Player, property_tree::ptree& data_json) {
     const OFFrame darkFrame;
     const OFStatus darkStatus;
 
-
-    for (property_tree::ptree::value_type& frame_data : data_json) {
-        property_tree::ptree frame_json = frame_data.second;
+    for (const json &frame_json : data_json) {
         vector<OFStatus> status;
         status.clear();
         status.resize(partNum);
-        fill(status.begin(), status.end(), darkStatus);  // fill 0 to every body part
+        fill(status.begin(), status.end(),
+             darkStatus);  // fill 0 to every body part
         vector<pair<string, OFStatus>> frameStatus;
         frameStatus.clear();
-        if (frame_json.size() == 0) {
+        if (frame_json.empty()) {
             // If no frames are given, push only dark frame
             // frameLists.push_back(darkFrame);
             // statusList.push_back(status);
-            cerr << "warning: OF has no frames" << endl;
             continue;
         }
-        const property_tree::ptree& status_json = frame_json.get_child("status");
-        for (unordered_map<string, int>::iterator part_it = Player.OFPARTS.begin(); part_it != Player.OFPARTS.end(); ++part_it) {
+        const json &status_json = frame_json["status"];
+        for (unordered_map<string, int>::iterator part_it =
+                 Player.OFPARTS.begin();
+             part_it != Player.OFPARTS.end(); ++part_it) {
             const string partName = part_it->first;
             const int id = part_it->second;
-            auto check = status_json.get_child_optional(partName);
-            if (!check) {
-                cerr << "Error in OF data: " << partName << " not found" << endl;
-            }
-            auto it = status_json.get_child(partName).begin();
+
             const OFStatus currentStatus(
-                it->second.get_value<int>(),
-                (++it)->second.get_value<int>(),
-                (++it)->second.get_value<int>(),
-                (++it)->second.get_value<int>());
+                status_json[partName][0], status_json[partName][1],
+                status_json[partName][2], (int)status_json[partName][3] * 10);
             status[id] = currentStatus;
             frameStatus.push_back(make_pair(partName, currentStatus));
         }
-        OFFrame currentFrame(frame_json.get<int>("start"), frame_json.get<bool>("fade"), frameStatus);
+        OFFrame currentFrame(frame_json["start"], frame_json["fade"],
+                             frameStatus);
         frameLists.push_back(currentFrame);
         statusList.push_back(status);
     }
@@ -247,7 +221,7 @@ void OFload(Player& Player, property_tree::ptree& data_json) {
         OFPlayer(Player.fps, frameLists, statusList, Player.OFPARTS, partNum);
     Player.OFloaded = true;
 
-    vector<int>& currentStatus = Player.myOFPlayer.currentStatus;
+    vector<int> &currentStatus = Player.myOFPlayer.currentStatus;
     currentStatus.resize(partNum);
     fill(currentStatus.begin(), currentStatus.end(), 0);
 }
